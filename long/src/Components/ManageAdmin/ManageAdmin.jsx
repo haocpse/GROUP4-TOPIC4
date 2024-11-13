2afc3264a4a262baf446de1bf5766cb73a3732b3
khoa231a <149999805+khoa231a@users.noreply.github.com>
2024-11-13 21:05:50 +0700

    Add Staff.jsx

--- long/src/Components/ManageAdmin/ManageAdmin.jsx
+++ long/src/Components/ManageAdmin/ManageAdmin.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import './ManageManager.module.css';
+import './ManageAdmin.module.css';
 import { Link, NavLink } from "react-router-dom";
 
 const ManageAdmin = () => {
