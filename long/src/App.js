f4681051bd09e7a5d62e033f34923ab862b1c698
Longnee <longthse184113@fpt.edu.vn>
2024-11-14 00:05:54 +0700

    Update App.js

--- long/src/App.js
+++ long/src/App.js
@@ -57,6 +57,7 @@ import BlogDetail from "./Components/Blog/BlodDetail";
 import CustomerViewMaintenance from "./Components/CustomerView/CustomerViewMaintenance";
 import Staff from "./Components/Package/Staff";
 import CustomerProfile from "./Components/CustomerProfile/CustomerProfile";
+import CustomerUpdate from "./Components/CustomerProfile/CustomerUpdate";
 function App() {
   return (
     <Router>
@@ -157,6 +158,7 @@ function App() {
 
         {/* CUSTOMER */}
         <Route path="/myInfo" element={<CustomerProfile />} />
+        <Route path="/myInfo/:accountId/update" element={<CustomerUpdate />} />
         <Route path="/myInfo/orders" element={<CustomerView />} />
         <Route path="/myInfo/orders/:constructionOrderId/quotation" element={<CustomerQuotationList />} />
         <Route path="/myInfo/orders/:constructionOrderId/design" element={<CustomerViewDesign />} />
